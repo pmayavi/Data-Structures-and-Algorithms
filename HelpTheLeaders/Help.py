b746e95fb11fd828a7cabdd8560698af7550aae6
pmayavi <68768011+pmayavi@users.noreply.github.com>
2022-02-20 18:39:04 -0500

    Add files via upload

--- HelpTheLeaders/Help.py
+++ HelpTheLeaders/Help.py
@@ -1,5 +1,4 @@
 from sys import stdin, stdout
-from itertools import combinations
 # Code by Pablo Maya Villegas
 # 14 February 2022
 
@@ -7,18 +6,22 @@ from itertools import combinations
 def Start():
     n = int(stdin.readline().strip())
     on = n + 1
-    string = ""
+    string = ''
 
     while n > 0:
-        combs = Set()
-        string = Save(string, on - n, combs)
+        string += 'Set ' + str(on - n) + ':\n'
+        string += Set()
+        string += '\n'
         n -= 1
-
-    # Writes a txt file with the correct combinations
     stdout.write(string)
 
-# The code for a single set, it gets the 3 inicial values and
-# two lists of the possible combinations and the prohibited combinations
+    # Code to print to a txt file the output for easy uDebug testing
+    #f = open("output.txt", "w")
+    #f.write(string)
+    #f.close()
+    
+# The code to read a single set, it gets the 3 inicial values and
+# two lists of the starter topics and the prohibited combinations
 def Set():
     ins = stdin.readline().strip().split(" ")
     t = int(ins[0])
@@ -28,10 +31,11 @@ def Set():
     start = []
     prohibited = []
     while t > 0:
-        start.append(stdin.readline()[:-1].upper())
+        start.append(stdin.readline().strip().upper())# Removes the \n character and upper cases it
         t -= 1
     start.sort()                     # Sorts the list alphabetically
     start.sort(key=len, reverse=True)# Sorts the list by size, longest is first
+    
     while p > 0:
         p1 = stdin.readline().strip().upper()
         p2 = p1.split(" ")
@@ -43,40 +47,50 @@ def Set():
     prohibited.sort(key=len, reverse=True)
 
 
-    return Combinations(start, prohibited, s)
-
-# Creates the possible combinations with the help of the itertool combinations
-# Removes the combinations that are prohibited
-def Combinations(start, prohibited, s):
-    c = combinations(start, s)
-    combs = [' '.join(i) for i in c]
-    remove = []
-    for c in combs:
-        for p in prohibited:
-            if p == c:
-                remove.append(c)
-            elif s >= 3:
-                t = True
-                z = p.split(" ")
-                for x in z:
-                    if (x + " ") not in (c + " "):
-                        t = False
-                if t:
-                    remove.append(c)
-
-    for r in remove:
-        if r in combs:
-            combs.remove(r)
-
-    return combs
-
-# Saves the Set to a string to be printed or written later
-def Save(string, n, combs):
-    string += "Set " + str(n) + ":\n"
-    for x in combs:
-        string += "".join(x) + "\n"
-    string += "\n"
+    t = len(start)
+    x = 0
+    A = [-1] * s
+    string = ''
+    # Starts the tree dividing it by all the possible starting topics
+    while x < t:
+        string += Recursion(A, x, s, t, 0, start, prohibited)
+        x += 1
     return string
 
+
+# Recursive class
+def Recursion(A, x, s, t, i, start, prohibited):
+    if A[i] == -1 and x < t:
+        A[i] = start[x] # Places the topic in the combination
+        x += 1    
+        string = ''
+        # If the desired s# combination is achived, cheks if its Valid
+        # Removes the last topic to backtrack to another branch and keep the same array
+        if s == 1:
+            string += Valid(' '.join(map(str, A)), len(A), prohibited)
+            A[i] = -1
+            return string
+
+        # Divides the tree by all the possible combinations of topics on the right
+        # This way topics on the left  don't get repeated
+        l = x
+        while l < t:
+            string += Recursion(A, l, s - 1, t, i + 1, start, prohibited)
+            l += 1
+
+        A[i] = -1 # Removes the topic to backtrack 
+        return string
+
+
+# Checks if the current combination is valid or prohibited
+def Valid(A, s, prohibited):
+    for p in prohibited:
+        if p == A:
+            return ''
+        elif s >= 3:
+            z = p.split(" ")
+            if (z[0] + " ") in (A + " ") and (z[1] + " ") in (A + " "):
+                return ''
+    return A + '\n' # It's valid
 
 Start()
