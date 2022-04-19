14de0a5ca15ce607ef86df4acbcd5255cd316f29
pmayavi <pmayav@eafit.edu.co>
2022-04-19 10:33:51 -0500

    Optimized Troublemakers

--- Troublemakers/Troublemakers.py
+++ Troublemakers/Troublemakers.py
@@ -1,6 +1,7 @@
 from sys import stdin, stdout 
-# Code by Pablo Maya Villegas
-# 4 April 2022
+# Code by Pablo Maya Villegas & Andres Echeverri Jaramillo
+# Created 4 April 2022 
+# Modified 18 April 2022
 
 # Reads the first line and starts a cicle for the number of sets
 def Start():
@@ -26,20 +27,19 @@ def Case():
     ins = stdin.readline().strip().split()
     n = int(ins[0])
     m = int(ins[1])
-    ma = m/2
 
-    trouble = []
-    while m > 0:
+    # List of strings where at location n are stored the prohibited pairs of that student
+    # Example, at if at trouble[8] is stored '3 5' then 8 3 and 8 5 are prohibited
+    trouble = [''] * (n+1)
+    loop = 0
+    while loop < m:
         x = stdin.readline().strip().split()
-        y = [int(x[0]), int(x[1])]
-        if y[0] > y[1]:
-            temp = y[0]
-            y[0] = y[1]
-            y[1] = temp
-        trouble.append(y)
-        m -= 1
-
-    return Recursion([1], [], n, ma, ma, 1, 0, 1, trouble)
+        y = sorted([int(x[0]), int(x[1])])
+        
+        trouble[y[1]] += str(y[0]) + ' '
+        loop += 1
+    
+    return Recursion(['1'], [], n, 1, 0, 1, trouble)
     
 # Recursive method
 # A & B = the two classrooms were students are stored
@@ -48,44 +48,39 @@ def Case():
 # la & lb = the size of the classrooms
 # start = the current student to be classified, starts at 1 and gets to n
 # trouble = the matrix of problematic pairs
-def Recursion(A, B, n, ma, mb, la, lb, start, trouble):
+def Recursion(A, B, n, la, lb, start, trouble):
     # If the classrooms are full or if all the students are assigned ends recursion
-    if la + lb == n or start > n:
+    if la + lb == n:
         string = str(la) + '\n'
         for a in A:
-            string += str(a) + ' '
+            string += a + ' '
         return string.strip()
 
     # Gets the weight (number of prohibited pairs) of the current number (start) in both classrooms
     start += 1
     weightA = 0
     for a in A:
-        for t in trouble:
-            if a == t[0] and start == t[1]:
-                weightA += 1
+        if a in trouble[start]:
+            weightA += 1
     weightB = 0
-    for a in B:
-        for t in trouble:
-            if a == t[0] and start == t[1]:
-                weightB += 1
+    for b in B:
+        if b in trouble[start]:
+            weightB += 1
 
     # If the weight is the same in both classrooms, the student is moved to the A class
     if weightA == weightB:
-        ma -= weightA
-        A.append(start)
+        A.append(str(start))
         la += 1
-        return Recursion(A, B, n, ma, mb, la, lb, start, trouble)
+        return Recursion(A, B, n, la, lb, start, trouble)
             
     # Moves the student to the classroom that has the least trouble pairs (lowest weight)
     if weightA > weightB:
-        mb -= weightB
-        B.append(start)
+        B.append(str(start))
         lb += 1
-        return Recursion(A, B, n, ma, mb, la, lb, start, trouble)
+        return Recursion(A, B, n, la, lb, start, trouble)
     else:
-        ma -= weightA
-        A.append(start)
+        A.append(str(start))
         la += 1
-        return Recursion(A, B, n, ma, mb, la, lb, start, trouble)
+        return Recursion(A, B, n, la, lb, start, trouble)
         
 Start()
